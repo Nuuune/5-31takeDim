f7c24e9d3e7bc3b7737970999629cf9bf85ba26e
nuuune <971061461@qq.com>
2018-06-04 18:15:40 +0800

    初步完成课程模块

---
+++ pages/course/csbuy/index.js
@@ -0,0 +1,9 @@
+
+
+let pageConfig = {
+  radioChange: function(e) {
+    console.log('radio发生change事件，携带value值为：', e.detail.value)
+  }
+};
+
+Page(pageConfig);
--- pages/course/csdetail/index.js
+++ pages/course/csdetail/index.js
@@ -1,6 +1,27 @@
+
+import constants from "../../../constants";
+
+let videoSrc = [
+    'http://glico.aosaiban.com/dist/video/video1.mp4',
+    'http://glico.aosaiban.com/dist/video/video2.mp4',
+    'http://glico.aosaiban.com/dist/video/video3.mp4'
+  ];
+
 let pageConfig = {
   data: {
-    istab1: true
+    istab1: false,
+    avater: constants.DEFAULT_AVATAR,
+    currVideo: ''
+  },
+  toggleTab: function(e) {
+    this.setData({istab1: !this.data.istab1});
+  },
+  onLoad: function() {
+    this.setData({currVideo: videoSrc[0]});
+  },
+  changeVideo: function(e) {
+    let vid = e.currentTarget.dataset.vid;
+    this.setData({currVideo: videoSrc[vid]});
   }
 
 };
